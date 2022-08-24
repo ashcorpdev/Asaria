90b529087795d662ce2e07c7922cb403fadf87f8
Ashen <ashen@ashcorp.dev>
2022-08-24 13:11:44 +0100

    Cleaned up database logging. Added new function.

--- backend/src/db/Database.ts
+++ backend/src/db/Database.ts
@@ -1,4 +1,5 @@
 import { join } from 'path'
+import { P } from 'pino'
 import sqlite3 = require('sqlite3')
 import { sys } from 'typescript'
 import { logger } from '../utils/Logger'
@@ -12,7 +13,7 @@ const db = new sqlite3.Database(dbPath, (err: Error | null) =>
 function onConnectedToDB(err: Error | null): void {
   logger.info(`Loading database at ${dbPath})`)
   if (err instanceof Error) {
-    logger.info('Failed to open database! Err: \n', err)
+    logger.error('Failed to open database! Err: \n', err)
     sys.exit()
   } else {
     validateTableData()
@@ -20,7 +21,7 @@ function onConnectedToDB(err: Error | null): void {
 }
 
 function validateTableData(): void {
-  logger.info('Validating table data in database...')
+  logger.info('Validating table data...')
   db.serialize(() => {
     const allianceQuery =
       'CREATE TABLE if NOT EXISTS alliances(alliance_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, display_name CHAR(25) NOT NULL UNIQUE, points INTEGER(6));'
@@ -29,20 +30,20 @@ function validateTableData(): void {
     const eventQuery =
       'CREATE TABLE if NOT EXISTS events(event_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, start_date DATE, end_date DATE, display_name CHAR(50))'
 
-    createDatabaseTable(allianceQuery)
-    createDatabaseTable(userQuery)
-    createDatabaseTable(eventQuery)
+    createDatabaseTable('alliances', allianceQuery)
+    createDatabaseTable('users', userQuery)
+    createDatabaseTable('events', eventQuery)
     createAlliancesData()
   })
 }
 
-function createDatabaseTable(query: string): void {
+function createDatabaseTable(tableName: string, query: string): void {
   db.run(query, (res: sqlite3.RunResult, err: Error | null) => {
     if (err instanceof Error) {
-      logger.info('Failed to create table!')
+      logger.error(`Failed to create ${tableName} table!`)
       sys.exit()
     } else {
-      logger.info('Validated table data!')
+      logger.info(`Validated ${tableName} data!`)
     }
   })
 }
@@ -55,20 +56,19 @@ function createAlliancesData(): void {
     "INSERT INTO alliances (display_name, points) VALUES ('Shadow Grove', 0);"
   ]
   queries.forEach((query) => {
-    logger.info(`Running query...`)
+    logger.info(`Running alliance data creation query...`)
     db.run(query, (res: sqlite3.RunResult, err: Error | null) => {
       if (err instanceof Error) {
-        logger.info('Failed to insert data for alliance!')
+        logger.error('Failed to insert data for alliance!')
         sys.exit()
       } else {
-        logger.info('Validated alliance data!')
+        logger.info('Query successful.')
       }
     })
   })
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function getAllianceIdByDisplayName(allianceName: string): number | null {
+function getAllianceIdByName(allianceName: string): number | null {
   let allianceId: number
   db.serialize(() => {
     db.get(
@@ -81,6 +81,25 @@ function getAllianceIdByDisplayName(allianceName: string): number | null {
         logger.info(res.alliance_id)
         allianceId = res.alliance_id
         return allianceId
+      }
+    )
+  })
+  return null
+}
+
+function getAllianceNameById(allianceId: number): string | null {
+  let allianceName: string
+  db.serialize(() => {
+    db.get(
+      `SELECT display_name FROM alliances WHERE alliance_id = "${allianceId}"`,
+      (err: Error | null, res: any) => {
+        if (err instanceof Error) {
+          logger.info('Failed to create table!')
+          return
+        }
+        logger.info(res.alliance_id)
+        allianceId = res.alliance_id
+        return allianceName
       }
     )
   })
--- backend/src/utils/Logger.ts
+++ backend/src/utils/Logger.ts
@@ -1,7 +1,7 @@
 import pino from 'pino'
 
 export const logger = pino({
-  name: process.env.APP_NAME,
+  name: 'Asaria',
   // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/prefer-nullish-coalescing
   level: 'info'
 })
